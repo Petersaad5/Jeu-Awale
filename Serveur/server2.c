be9503187b210c381e61c4e2c86aa11d55ac211e
Saad Peter <psaad@2D-LINUX-141.insa-lyon.fr>
2024-11-17 13:38:17 +0100

    multithreading implemented

--- Serveur/server2.c
+++ Serveur/server2.c
@@ -3,228 +3,94 @@
 #include <string.h>
 #include <errno.h>
 #include <time.h>
-#include "server2.h"
-#include "client2.h"
-#include "awale.h" // Include the header file for Awale functions
+#include <unistd.h>
 #include <sys/select.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "server2.h"
+#include "awale.h"
 
-
-
-static void init(void)
-{
+static void init(void) {
 #ifdef WIN32
-   WSADATA wsa;
-   int err = WSAStartup(MAKEWORD(2, 2), &wsa);
-   if (err < 0)
-   {
-      puts("WSAStartup failed!");
-      exit(EXIT_FAILURE);
-   }
+    WSADATA wsa;
+    int err = WSAStartup(MAKEWORD(2, 2), &wsa);
+    if (err < 0) {
+        puts("WSAStartup failed !");
+        exit(EXIT_FAILURE);
+    }
 #endif
 }
 
-static void end(void)
-{
+static void end(void) {
 #ifdef WIN32
-   WSACleanup();
+    WSACleanup();
 #endif
 }
 
-static void app(void)
-{
-   SOCKET sock = init_connection();
-   char buffer[BUF_SIZE];
-   int actual = 0;
-   int max = sock;
-   Client clients[MAX_CLIENTS];
+static int init_connection(void) {
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    SOCKADDR_IN sin = { 0 };
 
-   fd_set rdfs;
+    if (sock == INVALID_SOCKET) {
+        perror("socket()");
+        exit(errno);
+    }
 
-   while (1)
-   {
-      int i = 0;
-      FD_ZERO(&rdfs);
-      FD_SET(STDIN_FILENO, &rdfs);
-      FD_SET(sock, &rdfs);
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(PORT);
 
-      for (i = 0; i < actual; i++)
-      {
-         FD_SET(clients[i].sock, &rdfs);
-      }
+    if (bind(sock, (SOCKADDR *) &sin, sizeof(sin)) == SOCKET_ERROR) {
+        perror("bind()");
+        exit(errno);
+    }
 
-      if (select(max + 1, &rdfs, NULL, NULL, NULL) == -1)
-      {
-         perror("select()");
-         exit(errno);
-      }
+    if (listen(sock, MAX_CLIENTS) == SOCKET_ERROR) {
+        perror("listen()");
+        exit(errno);
+    }
 
-      if (FD_ISSET(STDIN_FILENO, &rdfs))
-      {
-         break;
-      }
-      else if (FD_ISSET(sock, &rdfs))
-      {
-         SOCKADDR_IN csin = {0};
-         socklen_t sinsize = sizeof(csin);
-         int csock = accept(sock, (SOCKADDR *)&csin, &sinsize);
-         if (csock == SOCKET_ERROR)
-         {
-            perror("accept()");
-            continue;
-         }
-
-         if (read_client(csock, buffer) == -1)
-         {
-            continue;
-         }
-
-         max = csock > max ? csock : max;
-         FD_SET(csock, &rdfs);
-
-         Client c = {csock};
-         strncpy(c.name, buffer, BUF_SIZE - 1);
-         c.name[BUF_SIZE - 1] = '\0'; // Ensure null-termination
-         c.status = AVAILABLE; // Default status
-         clients[actual] = c;
-         actual++;
-
-         char welcome_message[BUF_SIZE];
-         snprintf(welcome_message, BUF_SIZE, "Welcome %.50s! Type 'LIST' for online users or 'CHALLENGE:<pseudo>' to challenge someone.", c.name);
-         write_client(csock, welcome_message);
-      }
-      else
-      {
-         for (i = 0; i < actual; i++)
-         {
-            if (FD_ISSET(clients[i].sock, &rdfs))
-            {
-               Client client = clients[i];
-               int c = read_client(clients[i].sock, buffer);
-
-               if (c == 0)
-               {
-                  closesocket(clients[i].sock);
-                  remove_client(clients, i, &actual);
-                  snprintf(buffer, BUF_SIZE, "%.50s disconnected!", client.name);
-                  send_message_to_all_clients(clients, client, actual, buffer, 1);
-               }
-               else if (strncmp(buffer, "LIST", 4) == 0)
-               {
-                  send_online_clients_list(clients, actual, client.sock);
-               }
-               else if (strncmp(buffer, "CHALLENGE:", 10) == 0)
-               {
-                  // Challenge handling
-                  char opponent_name[BUF_SIZE];
-                  strncpy(opponent_name, buffer + 10, BUF_SIZE - 10); // Correct the offset to start after "CHALLENGE:"
-                  opponent_name[BUF_SIZE - 10] = '\0'; // Ensure null-termination
-
-                  handle_challenge(opponent_name, &clients[i], clients, actual);
-               }
-               else
-               {
-                  handle_challenge_response(clients, actual, i, buffer);
-               }
-               break;
-            }
-         }
-      }
-   }
-
-   clear_clients(clients, actual);
-   end_connection(sock);
+    return sock;
 }
 
-static int init_connection(void)
-{
-   SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-   SOCKADDR_IN sin = {0};
-
-   if (sock == INVALID_SOCKET)
-   {
-      perror("socket()");
-      exit(errno);
-   }
-
-   sin.sin_addr.s_addr = htonl(INADDR_ANY);
-   sin.sin_port = htons(PORT);
-   sin.sin_family = AF_INET;
-
-   if (bind(sock, (SOCKADDR *)&sin, sizeof(sin)) == SOCKET_ERROR)
-   {
-      perror("bind()");
-      exit(errno);
-   }
-
-   if (listen(sock, 10) == SOCKET_ERROR)
-   {
-      perror("listen()");
-      exit(errno);
-   }
-
-   return sock;
+static void end_connection(int sock) {
+    closesocket(sock);
 }
 
-static void end_connection(int sock)
-{
-   closesocket(sock);
+static int read_client(SOCKET sock, char *buffer) {
+    int n = recv(sock, buffer, BUF_SIZE - 1, 0);
+    if (n < 0) {
+        perror("recv()");
+        exit(errno);
+    }
+    buffer[n] = '\0';
+    return n;
 }
 
-static int read_client(SOCKET sock, char *buffer)
-{
-   int n = recv(sock, buffer, BUF_SIZE - 1, 0);
-   if (n < 0)
-   {
-      perror("recv()");
-      exit(errno);
-   }
-   buffer[n] = '\0';
-   return n;
+static void write_client(SOCKET sock, const char *buffer) {
+    if (send(sock, buffer, strlen(buffer), 0) < 0) {
+        perror("send()");
+        exit(errno);
+    }
 }
 
-static void write_client(SOCKET sock, const char *buffer)
-{
-   if (send(sock, buffer, strlen(buffer), 0) < 0)
-   {
-      perror("send()");
-      exit(errno);
-   }
+static void send_message_to_all_clients(Client *clients, int actual, const char *buffer, char from_server) {
+    for (int i = 0; i < actual; i++) {
+        write_client(clients[i].sock, buffer);
+    }
 }
 
-static void send_message_to_all_clients(Client *clients, Client sender, int actual, const char *buffer, char from_server)
-{
-   char message[BUF_SIZE];
-   message[0] = 0;
-   for (int i = 0; i < actual; i++)
-   {
-      if (sender.sock != clients[i].sock)
-      {
-         if (from_server == 0)
-         {
-            snprintf(message, BUF_SIZE, "%.50s: %s", sender.name, buffer);
-         }
-         else
-         {
-            snprintf(message, BUF_SIZE, "%s", buffer);
-         }
-         write_client(clients[i].sock, message);
-      }
-   }
+static void remove_client(Client *clients, int to_remove, int *actual) {
+    closesocket(clients[to_remove].sock);
+    memmove(clients + to_remove, clients + to_remove + 1, (*actual - to_remove - 1) * sizeof(Client));
+    (*actual)--;
 }
 
-static void remove_client(Client *clients, int to_remove, int *actual)
-{
-   closesocket(clients[to_remove].sock);
-   memmove(clients + to_remove, clients + to_remove + 1, (*actual - to_remove - 1) * sizeof(Client));
-   (*actual)--;
-}
-
-static void clear_clients(Client *clients, int actual)
-{
-   for (int i = 0; i < actual; i++)
-   {
-      closesocket(clients[i].sock);
-   }
+static void clear_clients(Client *clients, int actual) {
+    for (int i = 0; i < actual; i++) {
+        closesocket(clients[i].sock);
+    }
 }
 
 void handle_challenge(const char *target_name, Client *challenger, Client *clients, int actual) {
@@ -269,8 +135,21 @@ void handle_challenge_response(Client *clients, int actual, int i, const char *b
                 write_client(clients[i].sock, "Challenge accepted.\n");
                 write_client(clients[j].sock, "Challenge accepted.\n");
 
-                // Start the game
-                start_game(&clients[j], &clients[i]);
+                // Fork a new process for the game
+                pid_t pid = fork();
+                if (pid == 0) {
+                    // Child process
+                    start_game(&clients[j], &clients[i]);
+                    exit(0);
+                } else if (pid > 0) {
+                    // Parent process
+                    // Mark the clients as in-game
+                    clients[j].status = IN_GAME;
+                    clients[i].status = IN_GAME;
+                } else {
+                    perror("fork()");
+                    exit(errno);
+                }
 
                 break;
             }
@@ -410,6 +289,102 @@ const char* get_status_string(ClientStatus status) {
         default:
             return "Unknown";
     }
+}
+
+static void app(void) {
+    SOCKET sock = init_connection();
+    char buffer[BUF_SIZE];
+    int actual = 0;
+    Client clients[MAX_CLIENTS];
+
+    fd_set rdfs;
+
+    while (1) {
+        int i = 0;
+        FD_ZERO(&rdfs);
+        FD_SET(STDIN_FILENO, &rdfs);
+        FD_SET(sock, &rdfs);
+
+        int max_fd = sock;
+        for (i = 0; i < actual; i++) {
+            FD_SET(clients[i].sock, &rdfs);
+            if (clients[i].sock > max_fd) {
+                max_fd = clients[i].sock;
+            }
+        }
+
+        if (select(max_fd + 1, &rdfs, NULL, NULL, NULL) == -1) {
+            perror("select()");
+            exit(errno);
+        }
+
+        if (FD_ISSET(STDIN_FILENO, &rdfs)) {
+            // Handle server input
+            fgets(buffer, BUF_SIZE - 1, stdin);
+            buffer[strlen(buffer) - 1] = '\0';
+            if (strcmp(buffer, "QUIT") == 0) {
+                break;
+            } else if (strcmp(buffer, "LIST") == 0) {
+                send_online_clients_list(clients, actual, STDOUT_FILENO);
+            }
+        } else if (FD_ISSET(sock, &rdfs)) {
+            // Handle new connection
+            SOCKADDR_IN csin = { 0 };
+            socklen_t sinsize = sizeof(csin);
+            int csock = accept(sock, (SOCKADDR *)&csin, &sinsize);
+            if (csock == -1) {
+                perror("accept()");
+                continue;
+            }
+
+            if (actual >= MAX_CLIENTS) {
+                write_client(csock, "Server full\n");
+                close(csock);
+                continue;
+            }
+
+            Client c = { csock };
+            clients[actual] = c;
+            actual++;
+
+            // Read the client's name
+            if (read_client(csock, buffer) == -1) {
+                continue;
+            }
+            strncpy(clients[actual - 1].name, buffer, BUF_SIZE - 1);
+            clients[actual - 1].name[BUF_SIZE - 1] = '\0'; // Ensure null-termination
+            clients[actual - 1].status = AVAILABLE; // Default status
+
+            char welcome_message[BUF_SIZE];
+            snprintf(welcome_message, BUF_SIZE, "Welcome %.50s! Type 'LIST' for online users or 'CHALLENGE:<pseudo>' to challenge someone.\n", clients[actual - 1].name);
+            write_client(csock, welcome_message);
+        } else {
+            for (i = 0; i < actual; i++) {
+                if (FD_ISSET(clients[i].sock, &rdfs)) {
+                    int c = read_client(clients[i].sock, buffer);
+                    if (c == 0) {
+                        // Client disconnected
+                        remove_client(clients, i, &actual);
+                        continue;
+                    }
+
+                    // Handle client input
+                    if (strncmp(buffer, "CHALLENGE:", 10) == 0) {
+                        handle_challenge(buffer + 10, &clients[i], clients, actual);
+                    } else if (strcmp(buffer, "LIST") == 0) {
+                        send_online_clients_list(clients, actual, clients[i].sock);
+                    } else if (clients[i].is_challenged && strncmp(buffer, "ACCEPT", 6) == 0) {
+                        handle_challenge_response(clients, actual, i, buffer);
+                    } else if (clients[i].is_challenged && strncmp(buffer, "DENY", 4) == 0) {
+                        handle_challenge_response(clients, actual, i, buffer);
+                    }
+                }
+            }
+        }
+    }
+
+    clear_clients(clients, actual);
+    end_connection(sock);
 }
 
 int main(void) {
